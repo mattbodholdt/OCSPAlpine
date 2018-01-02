3e29fcfa6d53bf2c5072abd264041c699ea0f45e
mattbodholdt <31902842+mattbodholdt@users.noreply.github.com>
2018-01-02 00:37:22 -0600

    Update openssl_ocspd.sh

--- openssl_ocspd.sh
+++ openssl_ocspd.sh
@@ -2,5 +2,5 @@
 if [ -z "$PORT" }; then
 	echo "No port defined"
 else
-	openssl ocsp -sha256 -index /data/index.txt -CA /data/rsa_ca_chain.pem -rkey /data/ocsp_rsa.pem -rsigner /data/rsa_ocsp_cert.pem -ignore_err -timeout 120 -port 0.0.0.0:$PORT
+	openssl ocsp -sha256 -index /data/index.txt -CA /data/ca_chain.pem -rkey /data/ocsp_key.pem -rsigner /data/ocsp_cert.pem -ignore_err -timeout 120 -port 0.0.0.0:$PORT
 fi
